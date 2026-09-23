e17920ad48b788c6447baeb8a5b352f653f95627
Asifur Rahman <asifurrahman.ac@gmail.com>
2026-09-23 12:54:26 +0600

    problem one completed

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 ### 1.[Problem-1](./problem-1.js)
 ```
 1.Min, Max temperature
-2. Find wrong input types
+2.Find wrong input types
 3.Sub max - min
 ```
--- problem-1.js
+++ problem-1.js
@@ -1,1 +1,24 @@
-console.log('Hello')
+const temperature = [32, 35, "Hot", 30, 33, "Cold", 31, 36, 27, 34];
+
+const temperatureFn = (arr) => {
+  let min = arr[0];
+  let max = arr[0];
+  let sub = 0;
+  let invalidArr = [];
+
+  for (let i of arr) {
+    if (typeof i != "number") {
+      invalidArr.push(i);
+      continue;
+    }
+
+    min > i && (min = i);
+    max < i && (max = i);
+  }
+
+  sub = max - min;
+
+  return { min: min, max: max, sub: sub, invalidInputs: invalidArr };
+};
+
+console.log(temperatureFn(temperature));
